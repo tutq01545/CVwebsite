69d678a51e9df61bc689e134abe4f4973f0e99a5
ntnguyen96 <61455341+ntnguyen96@users.noreply.github.com>
2020-05-04 03:22:39 +0200

    Add Procfile and runtime.txt

--- Project/cv_website/cv_website/settings.py
+++ Project/cv_website/cv_website/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cv_website_app'
+    'cv_website_app',
+    'cv_website'
 ]
 
 MIDDLEWARE = [
